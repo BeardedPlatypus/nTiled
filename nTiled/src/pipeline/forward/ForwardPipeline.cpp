8c33e81411d4463f89fa2a129c5797c6238648c3
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2016-04-10 12:56:53 +0200

    Add pipeline loading of shaders

--- nTiled/src/pipeline/forward/ForwardPipeline.cpp
+++ nTiled/src/pipeline/forward/ForwardPipeline.cpp
@@ -2,30 +2,34 @@
 #include "pipeline\forward\shaders\ForwardAttenuatedShader.h"
 #include "pipeline\forward\shaders\ForwardTiledShader.h"
 
-#define VERT_PATH std::string("C:/Users/Monthy/Documents/projects/thesis/implementation_new/nTiled/nTiled/src/pipeline/shader-glsl/lambert_basic.vert")
-//#define FRAG_PATH std::string("C:/Users/Monthy/Documents/projects/thesis/implementation_new/nTiled/nTiled/src/pipeline/shader-glsl/lambert_basic_attenuated.frag")
-#define FRAG_PATH std::string("C:/Users/Monthy/Documents/projects/thesis/implementation_new/nTiled/nTiled/src/pipeline/shader-glsl/lambert_basic_tiled.frag")
+// Path defines
+#define VERT_PATH_BASIC std::string("C:/Users/Monthy/Documents/projects/thesis/implementation_new/nTiled/nTiled/src/pipeline/shader-glsl/lambert_basic.vert")
+#define FRAG_PATH_BASIC_ATTENUATED std::string("C:/Users/Monthy/Documents/projects/thesis/implementation_new/nTiled/nTiled/src/pipeline/shader-glsl/lambert_basic_attenuated.frag")
+#define FRAG_PATH_BASIC_TILED std::string("C:/Users/Monthy/Documents/projects/thesis/implementation_new/nTiled/nTiled/src/pipeline/shader-glsl/lambert_basic_tiled.frag")
 
 namespace nTiled {
 namespace pipeline {
 
 ForwardPipeline::ForwardPipeline(state::State& state) : Pipeline(state) { 
   for (ForwardShaderId id : this->state.shading.forward_shader_ids) {
-    // construct shader
-    ForwardShader* p_shader = new ForwardTiledShader(id,
-                                                     VERT_PATH,
-                                                     FRAG_PATH,
-                                                     *(this->state.p_world),
-                                                     this->state.view,
-                                                     glm::uvec2(32, 32));
-    /*
-    ForwardShader* p_shader = new ForwardAttenuatedShader(id,
-                                                          VERT_PATH,
-                                                          FRAG_PATH,
-                                                          *(this->state.p_world),
-                                                          this->state.view);
-                                                          */
-                                                          
+    ForwardShader* p_shader;
+
+    if (id == ForwardShaderId::ForwardAttenuated) {
+      p_shader = new ForwardAttenuatedShader(id,
+                                             VERT_PATH_BASIC,
+                                             FRAG_PATH_BASIC_ATTENUATED,
+                                             *(this->state.p_world),
+                                             this->state.view);
+    } else if (id == ForwardShaderId::ForwardTiled) {
+      p_shader = new ForwardTiledShader(id,
+                                        VERT_PATH_BASIC,
+                                        FRAG_PATH_BASIC_TILED,
+                                        *(this->state.p_world),
+                                        this->state.view,
+                                        glm::uvec2(32, 32));
+    } else {
+      throw std::runtime_error(std::string("Unsupported shader"));
+    }
     this->shader_catalog.push_back(p_shader);
   }
 }
