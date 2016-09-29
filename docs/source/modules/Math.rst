410c8470385dc4c7404b38c21e97874b5f43d9c2
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2016-09-29 17:01:11 +0200

    Add math module documentation

---
+++ docs/source/modules/Math.rst
@@ -0,0 +1,24 @@
+.. _Math:
+
+Math
+====
+
+The `Math` module contains all custom math functions used in `nTiled`.
+
+Math API
+--------
+
+Functions
+~~~~~~~~~
+
+.. doxygenfunction:: nTiled::math::to_radians
+
+.. doxygenfunction:: nTiled::math::to_degrees
+
+.. doxygenfunction:: nTiled::math::getNextPow2
+
+
+Constants
+~~~~~~~~~
+
+.. doxygenvariable:: nTiled::math::kPi
--- nTiled/include/math/util.h
+++ nTiled/include/math/util.h
@@ -1,29 +1,36 @@
+/*! @file util.h
+ *  @brief util.h contains utility math functions
+ */
+
 #pragma once
 
 namespace nTiled {
 namespace math {
 
+/*! @brief Constant value of pi. */
 constexpr double kPi = 3.141592653589793238462643383279502884197169399375105820974;
-/*! 
- * Convert the given value in degrees to radians
+
+/*! @brief Convert the given value in degrees to radians.
+ *  
+ *  @param x The value in degrees to be converted to radians.
+ *  @return The value of x in radians.
  */
 inline double to_radians(double x) { return (x * kPi) / 180.0; }
 
-/*!
- * Convert the given value in radians to degrees
+/*! @brief Convert the given value in radians to degrees.
+ * 
+ * @param x The value in radians to be converted in degrees.
+ * @return The value of x in degrees.
  */
 inline double to_degrees(double x) { return (x * (180 / kPi)); }
 
 // http://stackoverflow.com/questions/1322510/given-an-integer-how-do-i-find-the-next-largest-power-of-two-using-bit-twiddlin
-/*!
- * Get the next power of 2 of the given unsigned int.
+/*! @brief Get the next power of 2 of the given unsigned int.
  * 
- * param: 
- *     x (unsigned int): the input unsigned integer
+ * @param x the input unsigned integer.
  * 
- * return:
- *     the smallest power of 2 that is greater or equal to the given 
- *     integer
+ * @return the smallest power of 2 that is greater or equal to the given 
+ *         integer.
  */
 inline unsigned int getNextPow2(unsigned int x) {
   --x;
