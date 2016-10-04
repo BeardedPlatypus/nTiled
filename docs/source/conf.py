88504f5a80f825c29e1acfff8be6e625b8ae46f9
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2016-10-04 16:28:32 +0200

    Update docs/source/conf.py to enable ReadTheDocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -344,3 +344,12 @@ texinfo_documents = [
 breathe_projects = { "nTiled": "C:/Users/Monthy/Documents/projects/thesis/thesis-implementation/docs/doxyxml/" }
 
 breathe_default_project = "nTiled"
+
+
+import subprocess, os
+
+read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+
+if read_the_docs_build:
+
+    subprocess.call('cd ../doxygen; doxygen', shell=True)
