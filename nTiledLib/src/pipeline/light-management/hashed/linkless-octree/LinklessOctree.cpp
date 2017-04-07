5896b7589ccad9cb6330d95edecb2c777b5550e3
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2017-04-07 19:29:33 +0200

    Clean up test comments

--- nTiledLib/src/pipeline/light-management/hashed/linkless-octree/LinklessOctree.cpp
+++ nTiledLib/src/pipeline/light-management/hashed/linkless-octree/LinklessOctree.cpp
@@ -82,94 +82,22 @@ LinklessOctree::LinklessOctree(
     }
     this->gl_light_offset.push_back(light_offset);
   }
-  /*
-  // construct arrays of openGL data
-  this->p_octree_hash_maps_data_opengl = new GLubyte*[this->getNLevels()];
-  this->p_octree_hash_maps_offset_opengl = new GLubyte*[this->getNLevels()];
-
-  this->p_data_hash_maps_data_opengl = new GLuint*[this->getNLevels()];
-  this->p_data_hash_maps_offset_opengl = new GLubyte*[this->getNLevels()];
-
-  // copy data into openGL arrays
-  unsigned int data_index;
-
-  for (unsigned int i = 0; i < n_levels; i++) {
-    // ------------------------------------------------------------------------
-    //  p_octree_hash_maps_data_opengl
-    data_index = 0;
-
-    this->p_octree_hash_maps_data_opengl[i] =
-      new GLubyte[this->p_octree_hash_maps->at(i)->getHashTable().size() * 2];
-
-    for (glm::u8vec2 val : this->p_octree_hash_maps->at(i)->getHashTable()) {
-      this->p_octree_hash_maps_data_opengl[i][data_index++] = val.x;
-      this->p_octree_hash_maps_data_opengl[i][data_index++] = val.y;
-    }
-
-    // ------------------------------------------------------------------------
-    //  p_octree_hash_maps_offset_opengl
-    data_index = 0;
-
-    this->p_octree_hash_maps_offset_opengl[i] =
-      new GLubyte[this->p_octree_hash_maps->at(i)->getOffsetTable().size() * 3];
-
-    for (glm::u8vec3 val : this->p_octree_hash_maps->at(i)->getOffsetTable()) {
-      this->p_octree_hash_maps_offset_opengl[i][data_index++] = val.x;
-      this->p_octree_hash_maps_offset_opengl[i][data_index++] = val.y;
-      this->p_octree_hash_maps_offset_opengl[i][data_index++] = val.z;
-    }
-
-    if (this->p_data_hash_map_exists->at(i)) {
-      // ----------------------------------------------------------------------
-      // p_data_hash_maps_data_opengl
-      data_index = 0;
-
-      this->p_data_hash_maps_data_opengl[i] =
-        new GLuint[this->p_data_hash_maps->at(i)->getHashTable().size() * 2];
-
-      for (glm::uvec2 val : this->p_data_hash_maps->at(i)->getHashTable()) {
-        this->p_data_hash_maps_data_opengl[i][data_index++] = val.x;
-        this->p_data_hash_maps_data_opengl[i][data_index++] = val.y;
-      }
-
-      // ----------------------------------------------------------------------
-      // p_data_hash_maps_offset_opengl
-      data_index = 0;
-
-      this->p_data_hash_maps_offset_opengl[i] =
-        new GLubyte[this->p_data_hash_maps->at(i)->getOffsetTable().size() * 3];
-
-      for (glm::u8vec3 val : this->p_data_hash_maps->at(i)->getOffsetTable()) {
-        this->p_data_hash_maps_offset_opengl[i][data_index++] = val.x;
-        this->p_data_hash_maps_offset_opengl[i][data_index++] = val.y;
-        this->p_data_hash_maps_offset_opengl[i][data_index++] = val.z;
-      }
-    }
-  }
-  */
 }
 
 
 LinklessOctree::~LinklessOctree() {
-  // Remove openGL arrays
+  // Remove GPU datastructures
   // --------------------------------------------------------------------------
-  /*
-  for (unsigned int i = 0; i < this->getNLevels(); ++i) {
-    delete[] this->p_octree_hash_maps_data_opengl[i];
-    delete[] this->p_octree_hash_maps_offset_opengl[i];
-
-    if (this->p_data_hash_map_exists->at(i)) {
-      delete[] this->p_data_hash_maps_data_opengl[i];
-      delete[] this->p_data_hash_maps_offset_opengl[i];
-    }
+  GLsizei n_data_layers = 0;
+  for (bool v : *(this->p_data_hash_map_exists)) {
+    if (v) n_data_layers += 1;
   }
 
-  delete[] this->p_octree_hash_maps_data_opengl;
-  delete[] this->p_octree_hash_maps_offset_opengl;
+  glDeleteTextures(n_data_layers, ps_gfx_data_node_tables);
+  glDeleteTextures(n_data_layers, ps_gfx_data_offset_tables);
 
-  delete[] this->p_data_hash_maps_data_opengl;
-  delete[] this->p_data_hash_maps_offset_opengl;
-  */
+  glDeleteTextures(this->getNLevels(), ps_gfx_octree_node_tables);
+  glDeleteTextures(this->getNLevels(), ps_gfx_octree_offset_tables);
 
   //  Remove CPU datastructures
   // --------------------------------------------------------------------------
@@ -185,7 +113,6 @@ LinklessOctree::~LinklessOctree() {
   delete this->p_data_hash_maps;
 
   delete this->p_light_indices;
-
 }
 
 
@@ -282,52 +209,6 @@ void loadSpatialTable(GLuint* p_tex,
 
   GLint tex_uniform_loc = glGetUniformLocation(shader, glsl_sampler_name.c_str());
   glUniform1i(tex_uniform_loc, index);
-
-  /*
-  GLuint p_texture_data = glGetUniformLocation(shader, glsl_sampler_name.c_str());
-  glUniform1i(p_texture_data, index);
-
-  glActiveTexture(GL_TEXTURE0 + index);
-
-  glBindTexture(GL_TEXTURE_3D, p_texture);
-
-  glTexImage3D(GL_TEXTURE_3D,     // target
-               0,                 // mipmap level
-               internal_format,   // internal format: 2x8 bits unsigned integer, 2
-                                  // bits per node, 8 nodes packed per value
-               dimension,         // width
-               dimension,         // height
-               dimension,         // depth
-               0,                 // border
-               pixel_data_format, // pixel data format
-               pixel_data_type,   // pixel data type
-               data);
-  /*
-  glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-  glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-  glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-  glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-  glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_R, GL_REPEAT);
-  */
-  /*
-  glTexStorage3D(GL_TEXTURE_3D,
-                 1,
-                 internal_format,
-                 dimension,
-                 dimension,
-                 dimension);
-
-  glTexSubImage3D(GL_TEXTURE_3D,
-                  0, 0, 0, 0,
-                  dimension,
-                  dimension,
-                  dimension,
-                  pixel_data_format,
-                  pixel_data_type,
-                  data);
-
-  glBindTexture(GL_TEXTURE_3D, 0);
-  */
 }
 
 template void loadSpatialTable(GLuint*,
@@ -458,117 +339,6 @@ void LinklessOctree::loadToShader(GLuint shader) {
   GLuint p_octree_width = glGetUniformLocation(shader, "octree_width");
   float octree_width = float(this->getWidth());
   glUniform1f(p_octree_width, octree_width);
-
-  // --------------------------------------------------------------------------
-  //  Test values
-  /*
-  GLuint p_test;
-  glActiveTexture(GL_TEXTURE0 + 0);
-
-  glGenTextures(1, &p_test);
-  glBindTexture(GL_TEXTURE_2D, p_test);
-
-  glTexStorage2D(GL_TEXTURE_2D,
-                 1,
-                 GL_R8UI,
-                 1,
-                 1);
-
-  GLubyte test_dat[] = { 20 };
-  glTexSubImage2D(GL_TEXTURE_2D,
-                  0,
-                  0, 0,
-                  1, 1,
-                  GL_RED_INTEGER,
-                  GL_UNSIGNED_BYTE,
-                  test_dat);
-
-  /*
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, 0);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 0);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-  */
-
-  /*
-  GLuint p_sampler_test;
-  glGenSamplers(1, &p_sampler_test);
-
-  /*
-  glSamplerParameteri(p_sampler_test, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-  glSamplerParameteri(p_sampler_test, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-  glSamplerParameteri(p_sampler_test, GL_TEXTURE_WRAP_S, GL_REPEAT);
-  glSamplerParameteri(p_sampler_test, GL_TEXTURE_WRAP_T, GL_REPEAT);
-  */
-
-  /*
-  glBindSampler(0, p_sampler_test);
-  glBindTexture(GL_TEXTURE_2D, 0);
-
-  GLuint p_uniform_sampler_test = glGetUniformLocation(shader, "test");
-  glUniform1i(p_uniform_sampler_test, 0);
-  */
-  /*
-  GLuint p_test;
-  glGenTextures(1, &p_test);
-  glBindTexture(GL_TEXTURE_2D, p_test);
-
-  glTexStorage2D(GL_TEXTURE_2D,
-                 1,
-                 GL_RG8UI,
-                 1,
-                 1);
-
-  GLubyte test_dat[] = { 20, 15 };
-  glTexSubImage2D(GL_TEXTURE_2D,
-                  0,
-                  0, 0,
-                  1, 1,
-                  GL_RG_INTEGER,
-                  GL_UNSIGNED_BYTE,
-                  test_dat);
-
-  GLint test_uniform_loc = glGetUniformLocation(shader, "test");
-  glUniform1i(test_uniform_loc, 0);
-  glActiveTexture(GL_TEXTURE0);
-
-  glBindTexture(GL_TEXTURE_2D, p_test);
-  */
-  /*
-  GLuint p_test;
-  glGenTextures(1, &p_test);
-
-  glActiveTexture(GL_TEXTURE0 + 1);
-  glBindTexture(GL_TEXTURE_3D, p_test);
-
-  glTexStorage3D(GL_TEXTURE_3D,
-                 1,
-                 GL_RG8UI,
-                 2, 2, 2);
-
-  GLubyte test_dat[] = { 20, 15,
-                         19, 16,
-                         18, 17,
-                         17, 18,
-                         16, 19,
-                         15, 20,
-                         14, 21,
-                         13, 22,
-                       };
-
-  glTexSubImage3D(GL_TEXTURE_3D,
-                  0,
-                  0, 0, 0,
-                  2, 2, 2,
-                  GL_RG_INTEGER,
-                  GL_UNSIGNED_BYTE,
-                  test_dat);
-
-  GLint test_uniform_loc = glGetUniformLocation(shader, "test");
-  glUniform1i(test_uniform_loc, 1);
-  */
 }
 
 
