835eace5da80e5c5f0d6c19ee0b79222733db6f0
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2017-05-27 11:24:42 +0200

    Add nTiledData executable project

---
+++ nTiledData/src/nTiled.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#include "main\DataController.h"
+
+
+// ----------------------------------------------------------------------------
+//  Main
+// ----------------------------------------------------------------------------
+int main(int argc, char** argv) {
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << "<path_to_scene_def.json" << std::endl;
+    return -1;
+  }
+
+  nTiled::main::DataController controller =
+    nTiled::main::DataController(std::string(argv[1]));
+  
+  controller.init();
+  controller.execute();
+  controller.exportData();
+
+  return 0;
+}
